4e88e24d11949f43a24365cd8826814d7926bcbb
nestor <natalianestor@gmail.com>
2023-09-14 15:29:35 -0300

    agregandor estilos al componente novedades

--- src/components/Novesades/Novesades.js
+++ src/components/Novesades/Novesades.js
@@ -9,11 +9,13 @@ const Novesades = () => {
         <div className='reunion-de-padres'>
            <img src='/assets/reunion.jpg' alt='imagen'/>
            <h3>Reunion de Padres</h3>
-           <h4>3º grado: Viernes 01/09 19:30hs </h4>
-           <h4>2º grado: Lunes 04/09 11:00hs</h4>
-           <h4>1º grado: Martes 05/09 11:00hs</h4>
-           <h4>6º grado: Martes 12/09 07:30hs</h4>
-           <h4>7º grado: Martes 19/09 07:30hs</h4>
+           
+           <p><strong>3º grado:</strong> Viernes 01/09 19:30hs</p> <br/> 
+           <p><strong>2º grado:</strong> Lunes 04/09 11:00hs</p> <br/>
+           <p><strong>1º grado:</strong> Martes 05/09 11:00hs</p> <br/>
+           <p><strong>6º grado:</strong> Martes 12/09 07:30hs</p> <br/>
+           <p><strong>7º grado:</strong> Martes 19/09 07:30hs</p> 
+           
         </div>
         <div className='calendario'>
         <img src='/assets/calendario.jpg' alt='imagen'/>
@@ -27,15 +29,18 @@ const Novesades = () => {
         </div>
         <div className='viaje'>
         <img src='/assets/capiovi_img.jpeg' alt='imagen'/>
+        <h3>Viaje Capioví</h3>
+        <h4>Archivos para descargar</h4>
         <a href="assets/Autorizacion_Viaje_Capiovi.pdf">Autorización</a>
+        <a href="assets/nota_padres_Capiovi.pdf">Nota de padres</a>
         </div>
         <div className='libro'>
         <img src='/assets/libroingles.png' alt='imagen'/>
         <h3>Libros Ingles 2023</h3>
-        <p>1º grado: Storyline Starter A (Se comienza a usar el 1 de marzo)</p>
-        <p>2º grado: Storyline Starter B (Se comienza a usar a partir de 3/4. Durante el mes de marzo se termina de usar el del año anterior)</p>
-        <p>3º grado: Storyline 1 (Se comienza a usar a partir de 3/4. Durante el mes de marzo se termina de usar el del año anterior)</p>
-        <p>4º a 7º grado: Retirar el cuadernillo en Imprenta Heidell. Se utiliza desde el 1 de marzo.
+        <p><strong>1º grado:</strong> Storyline Starter A (Se comienza a usar el 1 de marzo)</p>
+        <p><strong>2º grado:</strong> Storyline Starter B (Se comienza a usar a partir de 3/4. Durante el mes de marzo se termina de usar el del año anterior)</p>
+        <p><strong>3º grado:</strong> Storyline 1 (Se comienza a usar a partir de 3/4. Durante el mes de marzo se termina de usar el del año anterior)</p>
+        <p><strong>4º a 7º grado:</strong> Retirar el cuadernillo en Imprenta Heidell. Se utiliza desde el 1 de marzo.
           
         </p>
   
